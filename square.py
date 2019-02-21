1de2947a2aa35d47f1a5d9d50a3f6828955b67e8
fairoos <fairoosnm123@gmail.com>
2019-02-21 11:43:07 +0530

    Apply formating up on file squre.py

--- square.py
+++ square.py
@@ -1,7 +1,8 @@
 def table(n):
     for i in range(1,n+1):
         for j in range(1,n+1):
-            print(i*j, end= ' ')
+            print("{:3d}".format(i*j), end = ' ')
+            #print(i*j, end= ' ')
             if j == n:
                 print("\n")
             
