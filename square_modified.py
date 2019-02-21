dd4521e781629f83dc1320b0f8666d93967573b7
fairoos <fairoosnm123@gmail.com>
2019-02-21 12:16:58 +0530

    Apply formatting up on table

--- square_modified.py
+++ square_modified.py
@@ -1,9 +1,9 @@
 def table(n):
     for i in range(1,n+1):
         if i ==2:
-            print ('-' * (n * 2))
+            print ('-' *3,'+',('-' *(n * 3)))
         for j in range(1,n+1):
-            print(i*j, end= ' ')
+            print("{:3d}".format(i*j), end = ' ')
             if j == 1:
                 print('|', end = '')
             if j == n:
